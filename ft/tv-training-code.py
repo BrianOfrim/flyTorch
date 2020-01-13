ece4c9349de8c4a067058f6230ab4d89b3e64476
BrianOfrim <bofrim@ualberta.ca>
2020-01-13 08:22:27 -0800

    Updates for less resource usage

--- ft/tv-training-code.py
+++ ft/tv-training-code.py
@@ -136,15 +136,20 @@ def main():
     dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
 
     # define training and validation data loaders
+    # data_loader = torch.utils.data.DataLoader(
+    #     dataset, batch_size=2, shuffle=True, num_workers=4, collate_fn=utils.collate_fn
+    # )
+
     data_loader = torch.utils.data.DataLoader(
-        dataset, batch_size=2, shuffle=True, num_workers=4, collate_fn=utils.collate_fn
+        dataset, batch_size=2, shuffle=True, num_workers=1, collate_fn=utils.collate_fn
     )
 
     data_loader_test = torch.utils.data.DataLoader(
         dataset_test,
         batch_size=1,
         shuffle=False,
-        num_workers=4,
+        # num_workers=4,
+        num_workers=1,
         collate_fn=utils.collate_fn,
     )
 
