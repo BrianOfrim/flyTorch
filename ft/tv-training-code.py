ba816c3976717a6136d4f4f9eef22278a62bb78f
BrianOfrim <bofrim@ualberta.ca>
2020-01-14 15:15:12 -0800

    [WIP] Workon converting torchvision tutorial

--- ft/tv-training-code.py
+++ ft/tv-training-code.py
@@ -4,6 +4,7 @@
 import os
 import time
 from typing import List
+import re
 
 from absl import app, flags
 import numpy as np
@@ -14,7 +15,9 @@ from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
 from engine import train_one_epoch, evaluate
 import utils
+import s3_util
 import transforms as T
+import pascal_voc_parser
 
 IMAGE_DIR_NAME = "images"
 ANNOTATION_DIR_NAME = "annotations"
@@ -42,47 +45,51 @@ flags.DEFINE_string("s3_data_dir", "data", "Prefix of the s3 data objects.")
 
 
 class ODDataSet(object):
-    def __init__(
-        self, data_root, transforms, labels: List[str], manifest_file_path: str
-    ):
+    def __init__(self, data_root, transforms, labels, manifest_file_path: str):
 
-        self.labels = labels
         self.data_root = data_root
         self.transforms = transforms
-        manifest_items = (
-            open(os.path.join(self.data_root, MANIFEST_DIR_NAME)).read().splitlines()
-        )
-        manifest_items = [item.strip() for item in manifest_items]
+
+        self.labels = labels
+        manifest_items = [
+            item.strip() for item in open(manifest_file_path).read().splitlines()
+        ]
         # Filter out Invalid images
         manifest_items = [
             item for item in manifest_items if item.split(",")[1].lower() != "Invalid"
         ]
-
-        self.images = [item.split(",")[0] for item in manifest_items]
-        self.annotations = [item.split(",")[1] for item in manifest_items]
+        self.images = [
+            os.path.join(self.data_root, IMAGE_DIR_NAME, item.split(",")[0])
+            for item in manifest_items
+        ]
+        self.annotations = [
+            os.path.join(self.data_root, ANNOTATION_DIR_NAME, item.split(",")[1])
+            for item in manifest_items
+        ]
 
     def __getitem__(self, idx):
         # load images ad masks
         img_path = os.path.join(self.data_root, IMAGE_DIR_NAME, self.images[idx])
         img = Image.open(img_path).convert("RGB")
 
-        num_objs = len(obj_ids)
-        boxes = []
-        for i in range(num_objs):
-            pos = np.where(masks[i])
-            xmin = np.min(pos[1])
-            xmax = np.max(pos[1])
-            ymin = np.min(pos[0])
-            ymax = np.max(pos[0])
-            boxes.append([xmin, ymin, xmax, ymax])
+        annotation_path = os.path.join(
+            self.data_root, ANNOTATION_DIR_NAME, self.annotations[idx]
+        )
+        _, annotation_boxes = pascal_voc_parser.read_content(annotation_path)
 
+        num_objs = len(annotation_boxes)
+        boxes = [[b.xmin, b.ymin, b.xmax, b.ymax] for b in annotation_boxes]
         boxes = torch.as_tensor(boxes, dtype=torch.float32)
         # there is only one class
-        labels = torch.ones((num_objs,), dtype=torch.int64)
-        masks = torch.as_tensor(masks, dtype=torch.uint8)
 
-        image_id = torch.tensor([idx])
-        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        labels = [self.labels.index(b.label) for b in annotation_boxes]
+        labels = torch.as_tensor(labels, dtype=torch.int64)
+
+        image_id = torch.as_tensor([idx])
+
+        area = [b.get_area() for b in annotation_boxes]
+        area = torch.as_tensor(area, dtype=torch.float32)
+
         # suppose all instances are not crowd
         iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
 
@@ -99,90 +106,7 @@ class ODDataSet(object):
         return img, target
 
 
-class PennFudanDataset(object):
-    def __init__(self, root, transforms):
-        self.root = root
-        self.transforms = transforms
-        # load all image files, sorting them to
-        # ensure that they are aligned
-        self.imgs = list(sorted(os.listdir(os.path.join(root, "PNGImages"))))
-        self.masks = list(sorted(os.listdir(os.path.join(root, "PedMasks"))))
-
-    def __getitem__(self, idx):
-        # load images ad masks
-        img_path = os.path.join(self.root, "PNGImages", self.imgs[idx])
-        mask_path = os.path.join(self.root, "PedMasks", self.masks[idx])
-        img = Image.open(img_path).convert("RGB")
-        # note that we haven't converted the mask to RGB,
-        # because each color corresponds to a different instance
-        # with 0 being background
-        mask = Image.open(mask_path)
-
-        mask = np.array(mask)
-        # instances are encoded as different colors
-        obj_ids = np.unique(mask)
-        # first id is the background, so remove it
-        obj_ids = obj_ids[1:]
-
-        # split the color-encoded mask into a set
-        # of binary masks
-        masks = mask == obj_ids[:, None, None]
-
-        # get bounding box coordinates for each mask
-        num_objs = len(obj_ids)
-        boxes = []
-        for i in range(num_objs):
-            pos = np.where(masks[i])
-            xmin = np.min(pos[1])
-            xmax = np.max(pos[1])
-            ymin = np.min(pos[0])
-            ymax = np.max(pos[0])
-            boxes.append([xmin, ymin, xmax, ymax])
-
-        boxes = torch.as_tensor(boxes, dtype=torch.float32)
-        # there is only one class
-        labels = torch.ones((num_objs,), dtype=torch.int64)
-        masks = torch.as_tensor(masks, dtype=torch.uint8)
-
-        image_id = torch.tensor([idx])
-        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
-        # suppose all instances are not crowd
-        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
-
-        target = {}
-        target["boxes"] = boxes
-        target["labels"] = labels
-        # target["masks"] = masks
-        target["image_id"] = image_id
-        target["area"] = area
-        target["iscrowd"] = iscrowd
-
-        if self.transforms is not None:
-            img, target = self.transforms(img, target)
-
-        return img, target
-
-    def __len__(self):
-        return len(self.imgs)
-
-
-def get_model_instance_segmentation(num_classes):
-    # load an instance segmentation model pre-trained pre-trained on COCO
-    # model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
-
-    # # get number of input features for the classifier
-    # in_features = model.roi_heads.box_predictor.cls_score.in_features
-    # # replace the pre-trained head with a new one
-    # model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-
-    # # now get the number of input features for the mask classifier
-    # in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
-    # hidden_layer = 256
-    # # and replace the mask predictor with a new one
-    # model.roi_heads.mask_predictor = MaskRCNNPredictor(
-    #     in_features_mask, hidden_layer, num_classes
-    # )
-
+def get_model_instance_detection(num_classes):
     # load a model pre-trained pre-trained on COCO
     model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
 
@@ -203,6 +127,39 @@ def get_transform(train):
     if train:
         transforms.append(T.RandomHorizontalFlip(0.5))
     return T.Compose(transforms)
+
+
+def get_files_from_dir(dir_path: str, file_type: str = None) -> List[str]:
+    if not os.path.isdir(dir_path):
+        return []
+    file_paths = [
+        f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))
+    ]
+    if file_type is not None:
+        file_paths = [f for f in file_paths if f.lower().endswith(file_type.lower())]
+    return file_paths
+
+
+def manifest_file_sort(manifest_file) -> int:
+    match = re.match("[0-9]+", manifest_file)
+    if not match:
+        return 0
+    return int(match[0])
+
+
+def get_newest_manifest_path(manifest_dir_path: str) -> str:
+    manifest_files = get_files_from_dir(manifest_dir_path)
+    manifest_files = [
+        f for f in manifest_files if f.lower().endswith(MANIFEST_FILE_TYPE)
+    ]
+    if len(manifest_files) == 0:
+        return None
+    newest_manifest_file = sorted(manifest_files, key=manifest_file_sort, reverse=True)[
+        0
+    ]
+    return os.path.join(
+        flags.FLAGS.local_data_dir, MANIFEST_DIR_NAME, newest_manifest_file
+    )
 
 
 def main(unused_argv):
@@ -266,27 +223,56 @@ def main(unused_argv):
         print("Invalid category labels path.")
         return
 
-    # read in the category labels
-    category_labels = open(flags.FLAGS.label_file_path).read().splitlines()
-    category_labels = [label.strip() for label in category_labels]
+    labels = [
+        label.strip() for label in open(flags.FLAGS.label_file_path).read().splitlines()
+    ]
 
-    if len(category_labels) == 0:
-        print("No label categories found")
+    if len(labels) == 0:
+        print("No labels are present in %s" % flags.FLAGS.label_file_path)
         return
+    newest_manifest_file = get_newest_manifest_path(
+        os.path.join(flags.FLAGS.local_data_dir, MANIFEST_DIR_NAME)
+    )
+
+    if newest_manifest_file is None:
+        print(
+            "Cannot find a manifest file in: %s"
+            % (os.path.join(flags.FLAGS.local_data_dir, MANIFEST_DIR_NAME))
+        )
 
     # train on the GPU or on the CPU, if a GPU is not available
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-    # our dataset has two classes only - background and person
-    num_classes = 2
+    # Add one class for the background
+
+    num_classes = len(labels) + 1
     # use our dataset and defined transformations
-    dataset = PennFudanDataset("PennFudanPed", get_transform(train=True))
-    dataset_test = PennFudanDataset("PennFudanPed", get_transform(train=False))
+    dataset = ODDataSet(
+        flags.FLAGS.local_data_dir,
+        get_transform(train=True),
+        labels,
+        newest_manifest_file,
+    )
+    dataset_test = ODDataSet(
+        flags.FLAGS.local_data_dir,
+        get_transform(train=False),
+        labels,
+        newest_manifest_file,
+    )
 
     # split the dataset in train and test set
     indices = torch.randperm(len(dataset)).tolist()
-    dataset = torch.utils.data.Subset(dataset, indices[:-50])
-    dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
+
+    # use 20 percent of the dataset for testing
+    num_test = int(0.2 * len(dataset))
+
+    dataset = torch.utils.data.Subset(dataset, indices[: -1 * num_test])
+    dataset_test = torch.utils.data.Subset(dataset_test, indices[-1 * num_test :])
+
+    print(
+        "Training dataset size: %d, Testing dataset size: %d"
+        % (len(dataset), len(dataset_test))
+    )
 
     # define training and validation data loaders
     # data_loader = torch.utils.data.DataLoader(
@@ -307,7 +293,7 @@ def main(unused_argv):
     )
 
     # get the model using our helper function
-    model = get_model_instance_segmentation(num_classes)
+    model = get_model_instance_detection(num_classes)
 
     # move model to the right device
     model.to(device)
